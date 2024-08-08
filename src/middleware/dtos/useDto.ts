dbdf1f21463d40e151b59176645f782491cbdf4f
Shubhcyb2611 <s.gupta@cybtekk.com>
2024-08-08 16:45:57 +0530

    feat: useDto added

---
+++ src/middleware/dtos/useDto.ts
@@ -0,0 +1,17 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { ZodSchema } from 'zod';
+
+export function useDto(schema: ZodSchema) {
+  return async (req: FastifyRequest, reply: FastifyReply) => {
+    try {
+      await schema.parseAsync(req.body);
+    } catch (error) {
+      reply.status(400).send({
+        error: {
+          code: 400,
+          message: error.errors[0].message,
+        },
+      });
+    }
+  };
+}
