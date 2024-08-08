b52aeb4cb67083a5217eada134f23c3e3ca21293
Shubhcyb2611 <s.gupta@cybtekk.com>
2024-08-08 16:53:00 +0530

    feat : logger added

---
+++ src/middleware/error/error.handler.ts
@@ -0,0 +1,21 @@
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { FastifyReply, FastifyRequest } from 'fastify';
+
+export function prismaErrorHandler(
+  err: any,
+  req: FastifyRequest,
+  reply: FastifyReply
+) {
+  let statusCode = 500;
+  if (err?.code) {
+    statusCode = parseInt(err.code.replace(/\D/g, ''), 10) || 500;
+
+  }
+  if(err?.meta?.cause === PrismaClientKnownRequestError){
+    if(err?.code === "P2002"){
+        statusCode = 409 ;
+    }
+  }
+  const errorMessage = err?.message || 'An unexpected error occurred';
+   Logger
+}
